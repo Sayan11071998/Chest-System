13f199b9bc04613ea65949205d21b9f7f4701cea
Sayan Nandi <sayannandi11071998@gmail.com>
2025-04-16 01:24:59 +0530

    New Notification Added

--- Assets/Scripts/Chest/States/UnlockingState.cs
+++ Assets/Scripts/Chest/States/UnlockingState.cs
@@ -1,5 +1,7 @@
 using ChestSystem.Chest.Core;
 using ChestSystem.Core;
+using ChestSystem.UI.Components;
+using ChestSystem.UI.Core;
 using ChestSystem.Utilities;
 using UnityEngine;
 
@@ -30,15 +32,44 @@ namespace ChestSystem.Chest
             // but we can use Update() for any continuous state-specific behaviors
         }
 
-        public void HandleChestClicked() => AttemptInstantUnlock();
+        public void HandleChestClicked() => ShowInstantUnlockNotification();
+
+        private void ShowInstantUnlockNotification()
+        {
+            int gemCost = chestController.Model.CurrentGemCost;
+            int playerGems = GameService.Instance.playerService.PlayerController.GemsCount;
+            string chestType = chestController.View.ChestType.ToString();
+
+            string title = $"INSTANT UNLOCK - {chestType} CHEST";
+            string message;
+
+            if (playerGems >= gemCost)
+            {
+                message = $"Would you like to instantly unlock this {chestType.ToLower()} chest for {gemCost} gems?\n\nYou have: {playerGems} gems\nCost: {gemCost} gems\n\nTap to confirm!";
+                NotificationManager.Instance.ShowNotification(title, message);
+                NotificationPanel.OnNotificationClosed += ConfirmInstantUnlock;
+            }
+            else
+            {
+                message = $"You don't have enough gems to instantly unlock this chest.\n\nYou have: {playerGems} gems\nRequired: {gemCost} gems\n\nWait for the timer or get more gems!";
+                NotificationManager.Instance.ShowNotification(title, message);
+            }
+        }
+
+        private void ConfirmInstantUnlock()
+        {
+            NotificationPanel.OnNotificationClosed -= ConfirmInstantUnlock;
+            AttemptInstantUnlock();
+        }
 
         private void AttemptInstantUnlock()
         {
             int playerGems = GameService.Instance.playerService.PlayerController.GemsCount;
+            int gemCost = chestController.Model.CurrentGemCost;
 
-            if (playerGems >= chestController.Model.CurrentGemCost)
+            if (playerGems >= gemCost)
             {
-                GameService.Instance.playerService.PlayerController.UpdateGemsCount(playerGems - chestController.Model.CurrentGemCost);
+                GameService.Instance.playerService.PlayerController.UpdateGemsCount(playerGems - gemCost);
                 CompleteUnlocking();
             }
             else
