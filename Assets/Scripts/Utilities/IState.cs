aa8312d563abde59b98004175bbaa1df50fddf6f
Sayan Nandi <sayannandi11071998@gmail.com>
2025-04-17 22:26:23 +0530

    IState Fixed

--- Assets/Scripts/Utilities/IState.cs
+++ Assets/Scripts/Utilities/IState.cs
@@ -5,6 +5,6 @@ namespace ChestSystem.Utilities
         public void OnStateEnter();
         public void Update();
         public void OnStateExit();
-        void HandleChestClicked();
+        public void HandleChestClicked();
     }
 }
