b73bc431ac99cc0daf257e357553fd405967fc9d
Sayan Nandi <sayannandi11071998@gmail.com>
2025-04-18 01:17:07 +0530

    UnlockingState Updated

--- Assets/Scripts/UI/Data/UIStrings.cs
+++ Assets/Scripts/UI/Data/UIStrings.cs
@@ -11,17 +11,22 @@ namespace ChestSystem.UI.Data
 
         #region Notification Popup Lebels
         public const string ChestLocked = "CHEST LOCKED";
+        public const string InstantUnlockChest = "INSTANT UNLOCK - {0} CHEST";
         #endregion
 
         #region Notification Popup Messages
         public const string AnotherChestIsAlreadyBeingUnlocked = "Another chest is already being unlocked!";
         public const string ChestRewards = "{0} CHEST REWARDS";
         public const string AboutToCollect = "You are about to collect:\n\n{0} coins\n{1} gems\n\nTap to collect!";
+        public const string WouldLikeInstantUnlock = "Would you like to instantly unlock this {0} chest for {1} gems?\n\nYou have: {2} gems\nCost: {1} gems\n\nTap to confirm!";
+        public const string DoNotHaveEnoughGems = "You don't have enough gems to instantly unlock this chest.\n\nYou have: {0} gems\nRequired: {1} gems\n\nWait for the timer or get more gems!";
         #endregion
 
         #region Notification Popup Button Lebels
         public const string Close = "CLOSE";
         public const string Collect = "COLLECT";
+        public const string Confirm = "CONFIRM";
+        public const string Okay = "OKAY";
         #endregion
     }
 }
